1084060e13bec4888914faa133ff437b4d7f4c9d
vath <346308443@qq.com>
2021-01-28 17:01:09 +0800

    'init'

---
+++ mall/src/main.js
@@ -0,0 +1,13 @@
+
+//引入插件在这里注册
+
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
